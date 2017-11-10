b41ab9793bc41d235c46d9524fc5f2d9b322627f
Pat Dayton <daytonpe14@gmail.com>
2017-11-10 09:24:45 -0600

    changed from word array to byte array

--- tictactoe_dayton.asm
+++ tictactoe_dayton.asm
@@ -15,23 +15,24 @@ oWins:	.asciiz "\n\nO Wins!\n\n"
 ws:	.asciiz "\n\n\n"
 pickle:	.asciiz "PICKLE RICK!"
 dubdub:	.asciiz "RUBALUBADUBDUB!"
+x:	.asciiz "X"
+o:	.asciiz "O"
 
-instrucArr:	.word '1','|','2','|','3','\n','4','|','5','|','6','\n','7','|','8','|','9'
+instrucArr:	.byte '1','|','2','|','3','\n','4','|','5','|','6','\n','7','|','8','|','9'
 	.space 100
 	
-moveArr:	.word '-','|','-','|','-','\n','-','|','-','|','-','\n','-','|','-','|','-'
+moveArr:	.byte '-','|','-','|','-','\n','-','|','-','|','-','\n','-','|','-','|','-'
 	.space 100
 
 turn:	.word 0 # 0--> O 1-->X
 
 ###################################################################################################################################################
 	.text
-main:	li $v0, 4	#print initial instructions
-  	la $a0, instruc1
-  	syscall
+main:	#li $v0, 4	#print initial instructions
+  	#la $a0, instruc1
+  	#syscall
   	
 	la $a1, instrucArr #print instruction board
-	li $a2, 17
 	jal printBoard
   	
   	li $v0, 4	#print white space
@@ -39,20 +40,29 @@ main:	li $v0, 4	#print initial instructions
   	syscall
   		
 	la $a1, moveArr #print the actual board
-	li $a2, 17
 	jal printBoard
 	
-	li $v0, 4	#print white space
-  	la $a0, ws
-  	syscall
+	#li $v0, 4	#print white space
+  	#la $a0, ws
+  	#syscall
   	
-  	li $v0, 4	
-  	la $a0, xTurn
-  	syscall
+  	#li $v0, 4	
+  	#la $a0, xTurn
+  	#syscall
   	
-  	lw $s6, turn
-  	jal switchTurn
+  	#lw $s6, turn
+  	#jal switchTurn
+  	#lw $s6, turn
+  	#jal switchTurn
   	
+  	#li $v0, 4	#print white space
+  	#la $a0, ws
+  	#syscall
+  	
+  	#li $a2, 1 #mark top left box
+  	#la $a1, moveArr #print the actual board
+  	#jal markBoard
+	#jal printBoard
 ###################################################################################################################################################	
 exit:	li $v0, 10
 	syscall
@@ -62,36 +72,51 @@ switchTurn:	li $t7, 1 #t7 = 1
 	beq $s6, $t7, oGoes 	#if turn==1 (X just went) branch
 	sw $t7, turn	#else set turn to 1 (O just went)
 	
-	li $v0, 4	
-  	la $a0, dubdub
-  	syscall
+#	li $v0, 4	
+# 	la $a0, dubdub
+#  	syscall
 	
 	jr $ra	
 		 
 oGoes:	li $t7, 0
 	sw $t7, turn	  #set turn to 0 (X just went)
 	
-	li $v0, 4	
-  	la $a0, pickle
-  	syscall
+#	li $v0, 4	
+#  	la $a0, pickle
+#  	syscall
   	
 	jr $ra
 	
-printBoard:	move $t0, $zero #i=0
-loop1:	sll $t1, $t0, 2 #bits = i*4
-	add $t2, $a1, $t1 #t2 = moveArray[i]
-	#sw $zero, ($t2) #moveArray[i]=0
+printBoard:		move $t0, $zero #i=0
+loop1:		sll $t1, $t0, 8 #convert i value to bytes
+		add $t2, $a1, $t1 #t2 = moveArray[i]
+		#sw $zero, ($t2) #moveArray[i]=0
 	
-	li $v0, 4
-  	la $a0, ($t2) #print each of the numbers in the loop
-  	syscall
+		li $v0, 4
+  		la $a0, ($t2) #print each of the numbers in the loop
+  		syscall
   	
-	addi $t0, $t0, 1 #i++
+		addi $t0, $t0, 1 #i++
 	
-	#jal divByThree
+		li $t5, 17 #array won't be longer than 17
+		slt $t3, $t0, $t5 #loop until we get to end
+		bne $t3, $zero, loop1
+		jr $ra
 	
-	slt $t3, $t0, $a2 #loop until we get to end
-	bne $t3, $zero, loop1
-	jr $ra
-	
-	
+makeMove: 		#based on the value that is passed in a2 this function: 
+		#1)prompts user for move based on who's turn it is
+		#2)takes in an integer value from 1-9
+		#4)calls the mark function
+		
+markBoard:		#based on value passed in via a0, this function:
+		#1)transform variable input so that it matches correct board position 1>>0, 8>>15
+		#2)marks the board in the correct position
+		#3)with the correct team (X/O)
+		
+		#a2=1
+		#a1=moveArray
+		sll $t1, $a2, 2 #i=a2*4, remember that a2 will be our transformed input
+		add $t2, $a1, $t1 #t2 = moveArray[i]
+		la $s0, x
+		#sw $s0, (t2)
+		jr $ra
